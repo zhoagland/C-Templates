9a4198dc1161b03cad33e9a6afa734cff64d8dc3
Zach Hoagland <zachary.hoagland@microchip.com>
2025-04-03 10:49:33 -0400

    Files with comment templates for breaking down files. 	new file:   File Templates/template.c 	new file:   File Templates/template.h

---
+++ File Templates/template.h
@@ -0,0 +1,35 @@
+/**
+ * @file template.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-04-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+/* Public Includes */
+
+/* Public Macros/Defines */
+
+/* Public Enums */
+
+/* Public Structs */
+
+/* Public (global) Vars */
+
+/* Public Function Declarations */
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif /* TEMPLATE_H */
